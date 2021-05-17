e3c46dcc11b7c1186fce2404c1a2026ee1cc0a46
Prabhakar <Prabhakar@DESKTOP-B3RBH50>
2021-05-17 11:47:12 +0530

    gittest 1 commit

---
+++ src/com/app/Test1.java
@@ -0,0 +1,10 @@
+package com.app;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hii");
+		System.out.println("bye");
+	}
+}
---
+++ src/com/app/Test2.java
@@ -0,0 +1,10 @@
+package com.app;
+
+public class Test2 {
+
+	static int a=10;
+	public static void main(String[] args) {
+		System.out.println("a is ");
+		System.out.println(a);
+	}
+}
